e01a9cb7aa80e15ed03c2dc1f49af5e223be65d1
Avinash <kumaravinash328@gmail.com>
2020-04-15 01:56:57 +0530

    Addition Program in JAVA

---
+++ Addition.java
@@ -0,0 +1,11 @@
+class Addition{
+	public static void main(String args[]){
+		int a = 100;
+		int b = 250;
+
+		int sum = a + b;
+
+		System.out.println("Sum = "+sum);
+		System.out.println("Sum of "+a+" + "+b+"="+sum);
+	}
+}
